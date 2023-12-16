34b1626e6cd0a5858b2ee33fe4a6df01cfc90255
ab992004 <115945395+ab992004@users.noreply.github.com>
2023-12-16 23:03:56 +0200

    Create Pyramic_Tic-Tac-Toe.cpp

---
+++ Pyramic_Tic-Tac-Toe.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <random>
+#include <iomanip>
+#include <algorithm>
+#include "BoardGame_Classes.hpp"
+using namespace std;
+
+// Set the board
+X_O_Board::X_O_Board () {
+   n_rows = 3;
+   n_cols = 5;
+   board = new char*[n_rows];
+   for (int i = 0; i < n_rows; i++) {
+       board [i] = new char[n_cols];
+       for (int j = 0; j < n_cols; j++)
+           board[i][j] = 0;
+   }
+}
+
+// Return true  if move is valid and put it on board
+// within board boundaries in empty cell
+// Return false otherwise
+bool X_O_Board::update_board (int x, int y, char mark){
+   // Only update if move is valid
+   if (((x == 0 && y == 2) || (x == 1 && (y >= 1 && y <= 3)) || (x == 2 && (y >= 0 && y <= 4)))  && (board[x][y] == 0)) {
+      board[x][y] = toupper(mark);
+      n_moves++;
+      return true;
+   }
+   else
+      return false;
+}
+// Display the board and the pieces on it
+void X_O_Board::display_board() {
+//   for (int i: {0,1,2}) {
+//       cout << "\n| ";
+//       for (int j: {0,1,2}) {
+//           cout << "(" << i << "," << j << ")";
+//           cout << setw(2) << board [i][j] << "|";
+//       }
+//       cout << "\n-----------------------------";
+//   }
+//   cout << endl;
+
+    cout << "\n";
+    cout << "               "<<'|';
+    cout << "(" << 0 << "," << 2 << ")";
+      cout << setw(2) << board [0][2] << " |";
+      cout << "\n-----------------------------\n";
+    cout << "\n";
+    cout << "         "<<'|';
+    cout << "(" << 1 << "," << 1 << ")";
+      cout << setw(2) << board [1][1] << " |";
+       cout << "(" << 1 << "," << 2 << ")";
+      cout << setw(2) << board [1][2] << " |";
+    cout << "(" << 1 << "," << 3<< ")";
+      cout << setw(2) << board [1][3] << " |";
+      cout << "\n-----------------------------\n";
+      cout <<"\n| ";
+    cout << "(" << 2 << "," << 0 << ")";
+      cout << setw(2) << board [2][0] << " |";
+    cout << "(" << 2 << "," << 1 << ")";
+      cout << setw(2) << board [2][1] << " |";
+    cout << "(" << 2 << "," << 2 << ")";
+      cout << setw(2) << board [2][2] << " |";
+    cout << "(" << 2 << "," << 3 << ")";
+      cout << setw(2) << board [2][3] << " |";
+    cout << "(" << 2 << "," << 4 << ")";
+      cout << setw(2) << board [2][4] << " |";
+      cout << "\n-----------------------------\n";
+}
+
+// Returns true if there is any winner
+// either X or O
+// Written in a complex way. DO NOT DO LIKE THIS.
+bool X_O_Board::is_winner() {
+   char row_win[3], col_win[3], diag_win[2];
+   for (int i:{0,1,2}) {
+       row_win[i] = board[i][0] & board[i][1] & board[i][2];
+       col_win[i] = board[0][i] & board[1][i] & board[2][i];
+   }
+   diag_win[0] = board[0][0] & board[1][1] & board[2][2];
+   diag_win[1] = board[2][0] & board[1][1] & board[0][2];
+
+   for (int i:{0,1,2}) {
+       if ( (row_win[i] && (row_win[i] == board[i][0])) ||
+            (col_win[i] && (col_win[i] == board[0][i])) )
+       {return true;}
+   }
+   if ((diag_win[0] && diag_win[0] == board[1][1]) ||
+       (diag_win[1] && diag_win[1] == board[1][1]))
+   {return true;}
+   if(board[0][2] != 0 && (board[0][2] == board[1][3] && board[0][2] == board[2][4])){
+       return true;
+   }   if(board[1][3] != 0 && (board[1][3] == board[1][2] && board[1][3] == board[1][1])){
+       return true;
+   }   if(board[2][4] != 0 && (board[2][4] == board[2][3] && board[2][4] == board[2][2])){
+       return true;
+   }
+
+   return false;
+
+}
+
+// Return true if 9 moves are done and no winner
+bool X_O_Board::is_draw() {
+   return (n_moves == 9 && !is_winner());
+}
+
+bool X_O_Board::game_is_over () {
+   return n_moves >= 9;
+}
